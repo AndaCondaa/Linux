ef6e89dee46f349ec64ba8f5a05724925d43504a
AndaCondaa <simon5229@naver.com>
2022-11-25 08:27:19 +0000

    1125

--- Linux/frame/mmap.c
+++ Linux/frame/mmap.c
@@ -5,6 +5,8 @@
 #include <sys/ioctl.h>
 #include <sys/types.h>
 #include <sys/mman.h>
+#include <math.h>
+
 
 #define FBDEVICE "/dev/fb0"
 
@@ -130,6 +132,39 @@ static int DrawFaceMMAP(int fd, int start_x, int start_y, int end_x, int end_y, 
 
     return 0;
 }
+
+static int DrawStart(int fd, int x, int y, unsigned short color)
+{
+    struct fb_var_screeninfo vinfo;
+    unsigned short *pfb;
+    double alpha = (2 * M_PI) / 10; 
+    int offset, radius = 12; 
+
+    /* 현재 프레임버퍼에 대한 고정된 화면 정보를 얻어온다. */
+    if(ioctl(fd, FBIOGET_VSCREENINFO, &vinfo) < 0) {
+        perror("Error reading fixed information");
+        return -1;
+    }
+ 
+    pfb = (unsigned short *)mmap(0, vinfo.xres_virtual*vinfo.yres_virtual*2, 
+                                    PROT_READ | PROT_WRITE,
+                                    MAP_SHARED, fd, 0);
+    for(int i = 11; i != 0; i—) {
+        double r = radius*(i % 2 + 1)/2;
+        double omega = alpha * i;
+        *(pfb + r * sin(omega) + x 
+              + (r * cos(omega) + y) * vinfo.xres_virtual) 
+    			= color;
+    }
+
+    munmap(pfb, vinfo.xres_virtual*vinfo.yres_virtual*2);
+
+    return 0;
+}
+
+
+
+
 
 int main(int argc, char** argv)
 {
