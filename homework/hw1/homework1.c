08045dddcbe72faebf5ee864e173cb7b7dc1a756
AndaCondaa <simon5229@naver.com>
2022-12-28 21:20:33 +0900

    homework1

--- homework/hw1/homework1.c
+++ homework/hw1/homework1.c
@@ -7,8 +7,11 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "bmpHeader.h"
+
+unsigned int paddingWidthBits(unsigned short bitCount, unsigned int width);
 
 int main(int argc, char **argv)
 {
@@ -17,6 +20,7 @@ int main(int argc, char **argv)
 	BITMAPFILEHEADER bmpHeader;
 	BITMAPINFOHEADER bmpInfoHeader;
 	unsigned char *image; 
+	unsigned int widthBits;
 
 	//Open the input file
 	file = fopen(argv[1], "rb");
@@ -26,27 +30,38 @@ int main(int argc, char **argv)
 	fread(&bmpInfoHeader, sizeof(BITMAPINFOHEADER), 1, file);
 
 	//Check the bit count whether it is 24bit or not 
-	if(bmpInfoHeader.biBitCount != 24) {
+	if (bmpInfoHeader.biBitCount != 24) {
 		perror("You can use only 24bit image\n");
 		return -1;	
 	}
-
 	
-	if (bmpInfoHeader.SizeImage == 0)
-		bmpInfoHeader.SizeImage = \
-			bmpInfoHeader.biWidth * bmpInfoHeader.biHeight * bmpInfoHeader.biBitCount / 8; 
+	widthBits = paddingWidthBits(bmpInfoHeader.biBitCount, bmpInfoHeader.biWidth);
 
-	image = (unsigned char*)malloc\
+	if (!bmpInfoHeader.SizeImage) {
+		bmpInfoHeader.SizeImage = widthBits * bmpInfoHeader.biHeight;
+	}
+	
+	image = (unsigned char*)malloc \
 		(sizeof(unsigned char) * bmpInfoHeader.SizeImage);
+	memset(image, 0, bmpInfoHeader.SizeImage);
 
 	fread(image, sizeof(unsigned char), bmpInfoHeader.SizeImage, file);
-	
-	for(int i=0; i < bmpInfoHeader.SizeImage; i+=bmpInfoHeader.biBitCount/8) {
-		printf("(%d, %d, %d)\n", image[i], image[i+1], image[i+2]);
-	}
-
 	fclose(file);
+	
+	for (int y=bmpInfoHeader.biHeight-1; y >= 0 ; y--) {
+		for (int x=0; x < bmpInfoHeader.biWidth; x++) { 
+			printf("(r = %3d, g = %3d, b = %3d)\n",
+	       			image[(x*3+2)+(y*widthBits)], image[(x*3+1)+(y*widthBits)], image[(x*3)+(y*widthBits)]);
+		}
+	}
 
 	return 0;
 }
 
+unsigned int paddingWidthBits(unsigned short bitCount, unsigned int width)
+{
+	unsigned int widthBits = bitCount * width;
+	unsigned int padWidth = ((widthBits+31)/32)*4;
+
+	return padWidth;
+}
